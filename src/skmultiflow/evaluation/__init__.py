e1bc6d3eb510ce4a5f94bc635b65a76b18f618d6
Fabricio Ceschin <fabricioceschin@gmail.com>
2020-03-11 23:24:16 +1300

    Started to implement evaluate_prequential_drift, which includes a drift detector in the cycle

--- src/skmultiflow/evaluation/__init__.py
+++ src/skmultiflow/evaluation/__init__.py
@@ -2,6 +2,7 @@
 The :mod:`skmultiflow.evaluation` module includes evaluation methods for stream learning.
 """
 
+from .evaluate_prequential_drift import EvaluatePrequentialDrift
 from .evaluate_prequential import EvaluatePrequential
 from .evaluate_holdout import EvaluateHoldout
 from .evaluation_data_buffer import EvaluationDataBuffer
